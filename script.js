b03cd3bf4ca834a0a6775b65cddfce3858e6e0c5
Chloe12250 <79823557+Chloe12250@users.noreply.github.com>
2021-04-12 14:49:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function math(){
+ var response = prompt("1+1=?");
+ if (response < 2){
+ alert("Try again!");
+}else if (response > 2){
+alert("Try again!");
+}else{
+alert("Bingo!");
+}
+}
+math();
+
+function math2(){
+ var response = prompt("1000-7=?");
+ if (response < 993){
+ alert("Try again!");
+}else if (response > 993){
+alert("Try again!");
+}else{
+alert("Bingo!");
+}
+}
+math2();
